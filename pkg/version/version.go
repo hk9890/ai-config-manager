245136e150e2e5861e0409b80a108385ec1f748c
Hans Kohlreiter <hans.kohlreiter@dynatrace.com>
2026-01-25 18:34:52 +0100

    chore: bump version to 1.7.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 // Version information - injected at build time
 var (
 	// Version is the semantic version number
-	Version = "1.4.0"
+	Version = "1.7.0"
 	// GitCommit is the git commit hash
 	GitCommit = ""
 	// BuildDate is the build date
